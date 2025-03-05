d6253be215b100565b07dff4fb631900d43ad1be
visruthasaripella <visruthapurnima1@gmail.com>
2025-03-05 16:33:57 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Chatbot",
+    version= "0.0.0",
+    author= "Visruta",
+    author_email= "visruthapurnima1@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+
+)
